66fd888450897e6cb773c959899cf81f7cb17b58
Arian Zhurovich <Arian@DESKTOP-SMOEV8A>
2025-09-27 22:04:34 +0300

    add messaging, dependency injection

---
+++ MaterialFlow.Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,7 @@
+﻿namespace MaterialFlow.Application.Abstractions.Messaging;
+
+public interface ICommand : IRequest<Result>, IBaseCommand;
+
+public interface ICommand<TReponse> : IRequest<Result<TReponse>>, IBaseCommand;
+
+public interface IBaseCommand;
---
+++ MaterialFlow.Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace MaterialFlow.Application.Abstractions.Messaging;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>;
