5c012a4492a44b57d869e4e0a5bee72b06c6e2cb
Arian Zhurovich <Arian@DESKTOP-SMOEV8A>
2025-09-27 20:17:39 +0300

    remove using

--- MaterialFlow.Domain/ForecastPlans/Events/ForecastPlanItemDeletedDomainEvent.cs
+++ MaterialFlow.Domain/ForecastPlans/Events/ForecastPlanItemDeletedDomainEvent.cs
@@ -1,7 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MaterialFlow.Domain.ForecastPlans.Events;
+﻿namespace MaterialFlow.Domain.ForecastPlans.Events;
 
 public sealed record ForecastPlanItemDeletedDomainEvent(Guid ItemId, Guid ForecastPlanId) : IDomainEvent;
