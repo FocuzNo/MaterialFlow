fb21f97505ceb32c410bfb05cd6236d48de7ce2f
Arian Zhurovich <Arian@DESKTOP-SMOEV8A>
2025-10-05 20:22:29 +0300

    rename layer

---
+++ MaterialFlow.Presentation/Endpoints/Materials/Create.cs
@@ -0,0 +1,19 @@
+﻿using MaterialFlow.Application.Materials.Commands.Create;
+
+namespace MaterialFlow.Presentation.Endpoints.Materials;
+
+internal sealed class CreateMaterialEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/materials", async (
+            CreateMaterialCommand request,
+            ISender sender) =>
+        {
+            var id = await sender.Send(request);
+
+            return Result.Create($"/materials/{id}");
+        })
+            .WithTags("Materials");
+    }
+}
