9a425bbc50f71819567145443908fff71e0a1d74
Arian Zhurovich <Arian@DESKTOP-SMOEV8A>
2025-10-05 19:44:35 +0300

    add infrastructure layer

---
+++ MaterialFlow.Infrastructure/Configurations/MaterialConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using MaterialFlow.Domain.Materials;
+using MaterialFlow.Domain.Materials.Enums;
+
+namespace MaterialFlow.Infrastructure.Configurations;
+
+public sealed class MaterialConfiguration : IEntityTypeConfiguration<Material>
+{
+    public void Configure(EntityTypeBuilder<Material> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.OwnsOne(x => x.MaterialNumber, x =>
+        {
+            x.Property(x => x.Value)
+            .HasMaxLength(50);
+        });
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(250);
+
+        builder.Property(x => x.BaseUnitOfMeasure)
+            .HasConversion(
+                x => x.Value,
+                x => new UnitOfMeasure(x))
+            .HasMaxLength(20);
+
+        builder.Property(x => x.MaterialRequirementsPlanningType)
+            .HasConversion(
+                x => x.Name,
+                x => MaterialRequirementsPlanningType.FromName(x))
+            .HasMaxLength(50);
+
+        builder.Property(x => x.LotSizePolicy)
+            .HasConversion(
+                x => x.Name,
+                x => LotSizePolicy.FromName(x))
+            .HasMaxLength(50);
+
+        builder.Property(x => x.ProcurementType)
+            .HasConversion(
+                x => x.Name,
+                x => ProcurementType.FromName(x))
+            .HasMaxLength(50);
+
+        builder.OwnsOne(x => x.SafetyStockQuantity, x =>
+        {
+            x.Property(p => p.Amount)
+             .HasPrecision(18, 3);
+        });
+
+        builder.Ignore(x => x.GetDomainEvents());
+    }
+}
---
+++ MaterialFlow.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using MaterialFlow.Domain.Abstractions;
+
+namespace MaterialFlow.Infrastructure.Repositories;
+
+public class Repository<TEntity>(DbContext dbContext)
+    : IRepository<TEntity> where TEntity : Entity
+{
+    private readonly DbContext _dbContext = dbContext;
+    private readonly DbSet<TEntity> _dbSet = dbContext.Set<TEntity>();
+
+    public async Task<TEntity?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        return await _dbSet
+            .FindAsync(
+                [id],
+                cancellationToken);
+    }
+
+    public async Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbSet
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task AddAsync(
+        TEntity entity,
+        CancellationToken cancellationToken = default)
+    {
+        await _dbSet.AddAsync(
+            entity,
+            cancellationToken);
+    }
+
+    public void Update(TEntity entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public void Delete(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+    }
+}
