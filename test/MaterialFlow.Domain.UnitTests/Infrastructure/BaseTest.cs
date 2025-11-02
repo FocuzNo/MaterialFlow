cf7866a98d094ecfd508770ce9072a7264292a7f
Arian Zhurovich <Arian@DESKTOP-SMOEV8A>
2025-11-02 20:53:05 +0300

    add unit test, add first test for users

---
+++ test/MaterialFlow.Domain.UnitTests/Infrastructure/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using MaterialFlow.Domain.Abstractions;
+
+namespace MaterialFlow.Domain.UnitTests.Infrastructure;
+
+public abstract class BaseTest
+{
+    protected static T AssertDomainEventWasPublished<T>(Entity entity) where T : IDomainEvent
+    {
+        var domainEvent = entity
+            .GetDomainEvents()
+            .OfType<T>()
+            .SingleOrDefault();
+
+        return domainEvent ?? throw new InvalidOperationException($"Domain event {typeof(T).Name} was not published.");
+    }
+}
